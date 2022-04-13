a78bd94f52f621c7e33ed54a46d8aae34da3927d
jfiora <sillonpeludo@gmail.com>
2022-04-13 16:39:57 -0300

    fix: icon

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 (function () {
-    let objRef = document.body;
+    const objRef = document.body;
     const url = window.location.href;
     if (checkUrl(url)) {
         objRef.style.filter = "blur(15px)";
     }
 })();
 
+
 function checkUrl(_url) {
-    let arr = ["https://twitter.com", "https://www.facebook.com", "https://www.instagram.com", "https://www.tiktok.com"];
+    const arr = ["https://twitter.com", "https://www.facebook.com", "https://www.instagram.com", "https://www.tiktok.com"];
     return arr.some(e => _url.startsWith(e));
 }
